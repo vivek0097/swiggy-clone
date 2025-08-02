0a5e3fab9569d939493b3ef680a639254698befd
vivek Singh <vek.sgh@gmail.com>
2025-08-02 23:47:39 +0530

    updating code

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -3,10 +3,16 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom"
 
 
+
+
+
+
 test("Should load Contact us component", () => {
  render(<Contact />);
 
  const heading = screen.getByRole("heading");
+
+ 
 //assertion---
  expect(heading).toBeInTheDocument();
 });
