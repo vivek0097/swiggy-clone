2a87ded5371031cdf11ffef1333c60aee139ade0
vivek Singh <vek.sgh@gmail.com>
2025-08-03 23:52:40 +0530

    writing testing for contact us  page  for  input ad  buttons

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -5,15 +5,48 @@ import "@testing-library/jest-dom"
 
 
 
+describe("Contact Us Page Test Case", () => {
 
 
-test("Should load Contact us component", () => {
+    test("Should load Contact us component", () => {
  render(<Contact />);
 
  const heading = screen.getByRole("heading");
 
- 
 //assertion---
  expect(heading).toBeInTheDocument();
 });
+
+
+it("Should load button inside Contact us component", () => {
+ render(<Contact />);
+
+ const button = screen.getByRole("button");
+
+//assertion---
+ expect(button).toBeInTheDocument();
+});
+
+it("Should load input inside Contact us component", () => {
+ render(<Contact />);
+
+ const input = screen.getAllByRole("input");
+
+//assertion---
+ expect(input).toBeInTheDocument();
+});
+
+
+it("Should load 2 input inside Contact us component", () => {
+ render(<Contact />);
+
+ const inputBoxes = screen.getAllByRole("textbox");
+
+//assertion---
+ expect(inputBoxes.length).not.toBe(3);
+});
+})
+
+
+
 
