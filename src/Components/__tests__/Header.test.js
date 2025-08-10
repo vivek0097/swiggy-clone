e501841cdc4a35ac9eb70965cee53fe67da67870
vivek Singh <vek.sgh@gmail.com>
2025-08-10 23:28:21 +0530

    in the header  added  more test  case  for  cart an d login logout button

--- src/Components/__tests__/Header.test.js
+++ src/Components/__tests__/Header.test.js
@@ -1,22 +1,39 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
-import AppStore from "../../utils/AppStore"
-import Header from "../Header"
+import AppStore from "../../utils/AppStore";
+import Header from "../Header";
 import { BrowserRouter } from "react-router-dom";
-import "@testing-library/jest-dom"
+import "@testing-library/jest-dom";
 
-it("Shoud render Header Component with a login button, ", () => {
-    render(
-      <BrowserRouter>
-       <Provider store={AppStore}>
-            <Header />
-        </Provider>
-        </BrowserRouter> 
-);
+const renderHeader = () =>
+  render(
+    <BrowserRouter>
+      <Provider store={AppStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
 
- const loginButton = screen.getByRole("button");
-//  const loginButton = screen.getByText("Login");
- expect(loginButton).toBeInTheDocument();
+it("Should render Header Component with a login button", () => {
+  renderHeader();
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  expect(loginButton).toBeInTheDocument();
 });
 
+it("Should render Header Component with a Cart item", () => {
+  renderHeader();
+  const cartItems = screen.getByText(/Cart/);
+  expect(cartItems).toBeInTheDocument();
+});
 
+it("Should change Login button to Logout on click", () => {
+  renderHeader();
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  expect(loginButton).toBeInTheDocument();
+
+  fireEvent.click(loginButton); 
+
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+  expect(logoutButton).toBeInTheDocument();
+});
